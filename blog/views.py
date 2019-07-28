79592955bd9c5115200a2c1a7edad2b126002d03
Leevenjun <i_suchuan@163.com>
2019-07-28 22:28:22 +0800

    模板嵌套与引用

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,13 @@ def blog_list(request):
     context = {}
     context["blogs"] = Blog.objects.all()
     # context["blogs_count"] = Blog.objects.all().count()       显示博客总数
-    return render_to_response('blog_list.html', context)
+    return render_to_response('blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_pk):
     context = {}
     context['blog'] = get_object_or_404(Blog, pk=blog_pk)
-    return render_to_response('blog_detail.html', context)
+    return render_to_response('blog/blog_detail.html', context)
 
 
 def blogs_with_type(request, blog_type_pk):
@@ -20,4 +20,4 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context["blog_type"] = blog_type
-    return render_to_response('blogs_with_type.html', context)
+    return render_to_response('blog/blogs_with_type.html', context)
