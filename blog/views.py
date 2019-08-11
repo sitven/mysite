47067a7b618af0d91cee0906547361b96e17c345
Leevenjun <i_suchuan@163.com>
2019-08-11 09:08:00 +0800

    博客数量分类统计

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Blog, BlogType
+from django.db.models import Count
 from django.conf import settings
 
 
@@ -23,12 +24,20 @@ def get_blogs_list_commin_date(request, blogs_all_list):
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
+    # 获取日期归档对应的博客数量
+    blog_dates = Blog.objects.dates('created_time', 'month', order='DESC')
+    blog_dates_dict = {}
+    for blog_date in blog_dates:
+        blog_count = Blog.objects.filter(created_time__year=blog_date.year,
+                                         created_time__month=blog_date.month).count()
+        blog_dates_dict[blog_date] = blog_count
+
     context = {}
     context["blogs"] = page_of_blogs.object_list
     context["page_of_blogs"] = page_of_blogs
-    context["page_range"] = page_range
-    context["blog_types"] = BlogType.objects.all()
-    context["blog_dates"] = Blog.objects.dates('created_time', 'month', order='DESC')
+    context["page_range"] = page_range                                                  # 博客列表页数
+    context["blog_types"] = BlogType.objects.annotate(blog_count=Count('blog'))         # 获取博客分类的对应博客数量
+    context["blog_dates"] = blog_dates_dict                                             # 年月分类
     return context
 
 
