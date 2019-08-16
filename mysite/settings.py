09ef6a3e9c829ff592ed1584d8d6f8843f316d96
Leevenjun <i_suchuan@163.com>
2019-08-16 23:27:44 +0800

    设置数据库缓存

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,3 +137,11 @@ CKEDITOR_UPLOAD_PATH = 'upload/'
 
 # 自定义参数
 EACH_PAGE_BLOGS_NUMBER = 7      # 每页博客数量
+
+# 缓存设置
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
