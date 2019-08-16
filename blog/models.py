bcd7d4c04cb8e3d8329b9a3dbc4eeb846af2794d
Leevenjun <i_suchuan@163.com>
2019-08-16 23:28:17 +0800

    热门阅读博客排行及缓存提速

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation
 from ckeditor_uploader.fields import RichTextUploadingField
-from read_statistics.models import ReadNumExpandMethod
+from read_statistics.models import ReadNumExpandMethod, ReadDetail
 
 
 
@@ -20,6 +21,7 @@ class Blog(models.Model, ReadNumExpandMethod):
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)    # 博客分类
     content = RichTextUploadingField()                                      # 内容
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)           # 作者
+    read_details = GenericRelation(ReadDetail)
     created_time = models.DateTimeField(auto_now_add=True)                  # 创建时间
     last_updated_time = models.DateTimeField(auto_now=True)                 # 编辑时间
 
