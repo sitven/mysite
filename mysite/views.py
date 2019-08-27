294800786519c52c8761a37c7502d2f63c51f532
Leevenjun <i_suchuan@163.com>
2019-08-27 23:24:39 +0800

    优化登录功能，新增注册功能

--- mysite/views.py
+++ mysite/views.py
@@ -5,10 +5,12 @@ from django.utils import timezone
 from django.db.models import Sum
 from django.core.cache import cache
 from django.contrib import auth
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 from read_statistics.utils import get_seven_days_read_date, get_today_hot_data, get_yesterday_hot_data
 from blog.models import Blog
+from .forms import LoginForm, RegForm
 
 
 def get_7_days_hot_blogs():
@@ -39,18 +41,43 @@ def home(request):
     return render(request, 'home.html', context)
 
 
+# 登录
 def login(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    user = auth.authenticate(request, username=username, password=password)
-    referer = request.META.get("HTTP_REFERER", reverse('home'))
-    if user is not None:
-        auth.login(request, user)
-        return redirect(referer)
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = login_form.cleaned_data['user']
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
     else:
-        return render(request, 'error.html', {'message': '用户名或密码不正确', 'redirect_to': referer})
+        login_form = LoginForm()
+
+    context = {}
+    context['login_form'] = login_form
+    return render(request, 'login.html', context)
 
 
+# 注册
+def register(request):
+    if request.method == 'POST':
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+            username = reg_form.cleaned_data['username']
+            email = reg_form.cleaned_data['email']
+            password = reg_form.cleaned_data['password']
+            # 创建用户
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            # 登录用户
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+    else:
+        reg_form = RegForm()
+
+    context = {}
+    context['login_form'] = reg_form
+    return render(request, 'login.html', context)
 
 
 
