2c59e1c515fef74cfcbc44b4b7d9c69d32ad8078
Leevenjun <i_suchuan@163.com>
2019-08-28 23:27:25 +0800

    评论使用富文本编辑器和ajax提交数据

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.contenttypes.models import ContentType
 from .models import Blog, BlogType
 from read_statistics.utils import read_statistics_once_read
 from comment.models import Comment
+from comment.forms import CommentForm
 
 
 def get_blogs_list_commin_date(request, blogs_all_list):
@@ -77,6 +78,7 @@ def blog_detail(request, blog_pk):
     context["next_blog"] = Blog.objects.filter(created_time__lt=blog.created_time).first()
     context["blog"] = blog
     context["comments"] = comments
+    context["comment_form"] = CommentForm(initial={"content_type": blog_content_type.model, "object_id": blog_pk})
     response = render(request, 'blog/blog_detail.html', context)    # 响应
     response.set_cookie(read_cookie_key, 'true')                    # 阅读cookie标记
     return response
