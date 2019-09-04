e9c760a1c3daa0c358a6995d23fc8be5e83b1c89
Leevenjun <i_suchuan@163.com>
2019-09-04 21:23:14 +0800

    实现点赞功能

--- blog/models.py
+++ blog/models.py
@@ -18,9 +18,9 @@ class BlogType(models.Model):
 # 博客信息
 class Blog(models.Model, ReadNumExpandMethod):
     title = models.CharField(max_length=50)                                 # 标题
-    blog_type = models.ForeignKey(BlogType, on_delete=models.CASCADE)    # 博客分类
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)    # 博客分类
     content = RichTextUploadingField()                                      # 内容
-    author = models.ForeignKey(User, on_delete=models.CASCADE)           # 作者
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)           # 作者
     read_details = GenericRelation(ReadDetail)
     created_time = models.DateTimeField(auto_now_add=True)                  # 创建时间
     last_updated_time = models.DateTimeField(auto_now=True)                 # 编辑时间
