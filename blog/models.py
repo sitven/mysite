66ae8e721c9b72a381f295fc6900da5d4ce19c67
Leevenjun <i_suchuan@163.com>
2019-09-15 09:22:14 +0800

    增加评论邮箱通知功能

--- blog/models.py
+++ blog/models.py
@@ -1,46 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericRelation
+from django.urls import reverse
 from ckeditor_uploader.fields import RichTextUploadingField
 from read_statistics.models import ReadNumExpandMethod, ReadDetail
 
-
-
-
-# 博客类型
 class BlogType(models.Model):
-    type_name = models.CharField(max_length=15)         # 博客类型名称
+    type_name = models.CharField(max_length=15)
 
     def __str__(self):
         return self.type_name
 
-
-# 博客信息
 class Blog(models.Model, ReadNumExpandMethod):
-    title = models.CharField(max_length=50)                                 # 标题
-    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)    # 博客分类
-    content = RichTextUploadingField()                                      # 内容
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)           # 作者
+    title = models.CharField(max_length=50)
+    blog_type = models.ForeignKey(BlogType, on_delete=models.CASCADE)
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     read_details = GenericRelation(ReadDetail)
-    created_time = models.DateTimeField(auto_now_add=True)                  # 创建时间
-    last_updated_time = models.DateTimeField(auto_now=True)                 # 编辑时间
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)    
 
+    def get_url(self):
+        return reverse('blog_detail', kwargs={'blog_pk': self.pk})
+
+    def get_email(self):
+        return self.author.email
 
     def __str__(self):
         return "<Blog: %s>" % self.title
 
     class Meta:
         ordering = ['-created_time']
-
-
-
-
-
-
-
-
-
-
-
-
-
